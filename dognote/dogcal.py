db4f10b8bcaccd35bce483fb1fca4ad8ead983da
Mark Harvey <markedwinharvey@gmail.com>
2017-01-28 16:41:21 -0800

    cal updates ;

--- dognote/dogcal.py
+++ dognote/dogcal.py
@@ -21,12 +21,15 @@ def get_cal(year,month,c):
 	cal = stream.getvalue()  	
 	return cal
 
-def format_cal(cal):
+def format_cal(cal,month,year):
 	'''create multidimensional array of calendar days'''
 	cal = [x.strip() for x in cal.split('\n') if x]
 	for line in range(len(cal)):
-		if line > 0:
+		if line == 1:
 			cal[line] = [y for y in cal[line].split(' ') if y]
+		if line > 1:
+			cal[line] = [[month,day,year] for day in cal[line].split(' ') if day]
+	#print cal
 	return cal
 
 def main(**kwargs):
@@ -41,15 +44,22 @@ def main(**kwargs):
 	
 	'''get calendars for current, previous, and next month'''
 	cal = get_cal(year,month,c)
-	cal = format_cal(cal)
+	cal = format_cal(cal,month,year)
 	
-	prev_cal = get_cal(year if month != 1 else year - 1, month - 1 if month != 1 else 12, c)
-	prev_cal = format_cal(prev_cal)
+	prev_year = year if month != 1 else year - 1
+	prev_month = month - 1 if month != 1 else 12
+	prev_cal = get_cal(prev_year, prev_month, c)
+	prev_cal = format_cal(prev_cal,prev_month,prev_year)
 	
-	next_cal = get_cal(year if month < 12 else year + 1, month + 1 if month < 12 else 1, c)
-	next_cal = format_cal(next_cal)
+	next_year = year if month < 12 else year + 1
+	next_month = month + 1 if month < 12 else 1
+	next_cal = get_cal(next_year, next_month, c)
+	next_cal = format_cal(next_cal,next_month,next_year)
 	
 	'''adjust cal to fill in month grid with extra days'''
+	
+	
+	
 	if len(cal[2]) < 7: 
 		cal[2] = prev_cal[-1] + cal[2] 
 	if len(cal[-1]) < 7:
@@ -57,10 +67,13 @@ def main(**kwargs):
 	if len(cal) < 7:		#handle 28-day February where Feb 1 is Sunday
 		cal.append(next_cal[2])
 	
-	cal[1] = cal[1]+cal[2]+cal[3]+cal[4]+cal[5]+cal[6]
+	cal[2] = cal[2]+cal[3]+cal[4]+cal[5]+cal[6]
+	cal = cal[:3]
 	#cal = cal[0] + [x for x in cal[1:]]
 	
+	#for i in cal:
+	#	print i
 	return cal
 
 if __name__ == '__main__':
-	main(**kwargs)
+	main()

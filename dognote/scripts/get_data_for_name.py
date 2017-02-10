54570bbb4a6c87cd89e161423eea1ccc02be94ab
Mark Harvey <markedwinharvey@gmail.com>
2017-02-10 13:40:27 -0800

    return "importance" with get_data function

--- dognote/scripts/get_data_for_name.py
+++ dognote/scripts/get_data_for_name.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
+'''
+get_data_for_name.py retrieves date data for selected name, as well as "important" (a boolean)
+
+'''
+
 import json
 import operator
 
@@ -7,9 +12,17 @@ def main(name):
 		data = json.loads(f.read())
 	dates = data['dates'].keys()
 
-	meeting_order = sorted( [x for x in data['dates']] , key=lambda k: data['dates'][k]['meeting_date_epoch_time'] , reverse=True )
-
-	return meeting_order
+	
+	m = sorted( [  x for x in data['dates']] , key=lambda k: data['dates'][k]['meeting_date_epoch_time'] , reverse=True )
+	#meeting order
+	
+	i = [ data['dates'][x]['important'] for x in m ] 
+	#importance order
+	
+	
+	final = [(m[x],i[x]) for x in range(len(m))]
+	return final
+	
 	
 if __name__ == '__main__':
 	main(name)
